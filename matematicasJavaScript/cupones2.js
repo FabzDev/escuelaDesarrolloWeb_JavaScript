1cfe40a46233f67ea07c0f76dd8d827b3a4fc008
fabztech <fabztech@outlook.com>
2022-09-08 19:15:20 -0500

    Iniciando archivos con cupones version platzi

---
+++ matematicasJavaScript/cupones2.js
@@ -0,0 +1,40 @@
+//DEFINIENDO VARIABLES DEL HTML
+const btn = document.querySelector("#btn");
+const inputTreatment = document.querySelector("#treatment");
+const inputCupon = document.querySelector("#cupon");
+const result = document.querySelector("#result");
+
+// CREANDO CLASE
+class Cupones {
+	constructor(name, value) {
+		this.cName = name;
+		this.cValue = value;
+	}
+}
+
+// OBJETOS BASE DE DATOS
+const cupon0 = new Cupones("skinby10", 10);
+const cupon1 = new Cupones("skinby30", 30);
+const cupon2 = new Cupones("skinby50", 50);
+
+// CREANDO ARRAY DE OBJETOS
+const cupones = [cupon0, cupon1, cupon2];
+
+// for (i = 0; i < 3; i++) {
+// 	cupones.push(cupon[i]);
+// } PENDIENTE POR SOLUCIONAR
+
+// LLAMANDO AL EVENTO CLICK
+btn.addEventListener("click", cuponFunction);
+
+// CICLO EJECUTOR (PAUSADO)
+// function cuponFunction() {
+// 	const price = inputTreatment.value;
+// 	for (vCupones of cupones) {
+// 		if (inputCupon.value == vCupones.cName) {
+// 			const cuponValue = vCupones.cValue;
+// 			const newPrice = price * ((100 - cuponValue) / 100);
+// 			result.innerHTML = "El precio con descuento es: $" + newPrice;
+// 		}
+// 	}
+// }
