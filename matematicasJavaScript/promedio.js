db0c00ccfb7ff676ffa58e8d3b9067330606eab9
fabztech <fabztech@outlook.com>
2022-09-09 22:24:14 -0500

    Metodo reduce para sacar promedio

--- matematicasJavaScript/promedio.js
+++ matematicasJavaScript/promedio.js
@@ -1,15 +1,22 @@
-const notas = [2, 3, 4];
+const notas = [200000, 3, 4];
 
-let suma = 0;
-let prom = 0;
+// let suma = 0;
+// let prom = 0;
 
-for (not of notas) {
-	suma += not;
-	prom = suma / notas.length;
+// for (not of notas) {
+// 	suma += not;
+// 	prom = suma / notas.length;
+// }
+
+// console.log(prom);
+
+// const resultado = document.querySelector("h1");
+
+const sumaLista = notas.reduce(sumarElements);
+
+function sumarElements(valorAcum, valorNew) {
+	return valorAcum + valorNew;
 }
 
-console.log(prom);
-
-const resultado = document.querySelector("h1");
-
-resultado.innerText = "Tu promedio de notas este corte es: " + prom;
+const promedio = sumaLista / notas.length;
+console.log("Tu promedio de notas en este corte es: " + promedio);
