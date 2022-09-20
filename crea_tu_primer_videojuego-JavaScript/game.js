2918586a9e50c3b8aba3eca6922ce5a5621a52de
fabztech <fabztech@outlook.com>
2022-09-19 21:14:03 -0500

    3573-javascript-practico-videojuegos/52330-objeto-playerposition V1.2 Calavera en movimiento empaquetando codigo en funciones

--- crea_tu_primer_videojuego-JavaScript/game.js
+++ crea_tu_primer_videojuego-JavaScript/game.js
@@ -17,14 +17,13 @@ const playerPosition = {
 };
 
 function startGame() {
-	context.clearRect(0, 0, canvasSize, canvasSize);
+	delMap();
 	context.textAlign = "end";
 	context.font = elementsSize + "px Verdana";
 
 	const map = maps[0];
 	const cleanMap = map.trim().split("\n");
 	const matrixMap = cleanMap.map((row) => row.trim().split(""));
-
 	matrixMap.forEach((row, rowP) => {
 		row.forEach((col, colP) => {
 			let posX = elementsSize * (colP + 1);
@@ -41,9 +40,14 @@ function startGame() {
 			context.fillText(emojis[col], posX, posY);
 		});
 	});
+	movePlayer();
+}
+function movePlayer() {
 	context.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
 }
-
+function delMap() {
+	context.clearRect(0, 0, canvasSize, canvasSize);
+}
 // context.fillRect(100, 100, 1, 1);
 // context.clearRect(50, 0, 100, 50);
 // context.font = "30px Cabin";
