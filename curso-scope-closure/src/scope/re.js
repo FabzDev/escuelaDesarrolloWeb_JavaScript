450b5c9a9c91647eda036cf80510dea25dfc8e2f
fabztech <fabztech@outlook.com>
2022-09-25 17:10:25 -0500

    3213-javascript-closures-scope/50362-function-scope/ apuntes clase 4

---
+++ curso-scope-closure/src/scope/re.js
@@ -0,0 +1,29 @@
+// VAR
+
+var firstName; // undefined
+firstName = "Oscar";
+console.log(firstName);
+
+var lastName = "David"; // declarar / asignar
+lastName = "Ana"; // reasignar
+
+var secondName = "David"; // declarar / asignar
+var secondName = "Ana"; // redeclarar / reasignar
+
+// LET
+
+let fruit = "Apple"; //declarar / asignar
+fruit = "Kiwi;"; // reasignar
+
+// let fruit = "Banana"; redeclarar (ERROR)
+
+//CONST
+
+const animal = "Dog"; // declaracion y asignacion
+// animal = "Gato"; reasignando (ERROR)
+// const animal = "Snake"; redeclarando y rasingnando (ERROR)
+
+const vehicles = [];
+
+vehicles.push("Renault");
+vehicles.pop("Renault");
