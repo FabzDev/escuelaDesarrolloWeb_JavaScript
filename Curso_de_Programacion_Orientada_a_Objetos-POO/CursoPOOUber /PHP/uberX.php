27c09149cbd6d81c00cdf2b670e59a16c312e84c
fabztech <fabztech@outlook.com>
2022-10-01 12:44:09 -0500

    modificado nombre de carpeta curso de programacion para eliminar espacios / pospuesto curso de programacion orientada a objetos con diferentes lenguajes

--- Curso_de_Programacion_Orientada_a_Objetos-POO/CursoPOOUber /PHP/uberX.php
+++ Curso_de_Programacion_Orientada_a_Objetos-POO/CursoPOOUber /PHP/uberX.php
@@ -1,4 +1,5 @@
 <?php
+require_once('car.php');
 class uberX extends Car {
   public $brand;
   public $model;
---
+++ Curso_de_Programacion_Orientada_a_Objetos-POO/CursoPOOUber /Python/UberBlack.py
@@ -0,0 +1,5 @@
+from car import Car
+
+class UberX(Car):
+  brand = str
+  model = str
