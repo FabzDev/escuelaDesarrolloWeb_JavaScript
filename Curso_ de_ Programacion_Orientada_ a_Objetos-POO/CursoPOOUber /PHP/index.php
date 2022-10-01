5d3a1772e85a7d49cbccb5d9dac1d88567aa931d
fabztech <fabztech@outlook.com>
2022-10-01 12:38:20 -0500

    subclases driver agregadas a PHP

--- Curso_ de_ Programacion_Orientada_ a_Objetos-POO/CursoPOOUber /PHP/index.php
+++ Curso_ de_ Programacion_Orientada_ a_Objetos-POO/CursoPOOUber /PHP/index.php
@@ -1,8 +1,10 @@
 <?php
+require_once('car.php');
+require_once('uberX.php');
+require_once('account.php');
 
-$
+$uberX = new UberX("KHX416", new Account("Fabio Escobar", "1140"), "Renault", "Sandero");
 
-abstract class Account {
-  protected $name;
+$uberX->printDataCar();
 
-}
+?> 
