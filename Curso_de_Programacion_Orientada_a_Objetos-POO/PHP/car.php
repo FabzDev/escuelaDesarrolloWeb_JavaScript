d326730a02633578377d9c617c26638bb771eb94
fabztech <fabztech@outlook.com>
2022-10-01 21:08:15 -0500

    Metodos constructores creados en PHP por mi cuenta

--- Curso_de_Programacion_Orientada_a_Objetos-POO/PHP/car.php
+++ Curso_de_Programacion_Orientada_a_Objetos-POO/PHP/car.php
@@ -1,11 +1,21 @@
 <?php
-class Car{
+
+class Car extends Account{
 public $id;
 public $license;
 public $driver;
 public $passanger;
 
+
+
+public function __construct($license, $driver){
+  $this->license = $license;
+  $this->$driver = $driver;
 }
 
+public function printDataCar(){
+  echo $this->license;
+}
 
+}
 ?>
---
+++ Curso_de_Programacion_Orientada_a_Objetos-POO/PHP/main.php
@@ -0,0 +1,4 @@
+<?php
+
+$car1 = new Car("KHX416", new Account("Fabio Escobar", "1140"))
+car1.printDataCar();
