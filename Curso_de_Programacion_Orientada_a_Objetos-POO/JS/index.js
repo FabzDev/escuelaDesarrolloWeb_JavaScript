9864161c84af8d958ad583b4ccb63da8f4af364b
fabztech <fabztech@outlook.com>
2022-10-01 20:51:59 -0500

    Metodos constructores creados en PYTHON

--- Curso_de_Programacion_Orientada_a_Objetos-POO/JS/index.js
+++ Curso_de_Programacion_Orientada_a_Objetos-POO/JS/index.js
@@ -4,7 +4,9 @@
 // console.log("Car licence: " + car1.license + " Driver: " + car1.driver);
 
 var car1 = new Car("KHX416", new Account("Fabio Escobar", "1140"));
+car1.passenger = 4;
 car1.printDataCar();
 
 var car2 = new Car("XHK416", new Account("Alejandro Escobar", "4011"));
+car1.passenger = 3;
 car2.printDataCar();
--- Curso_de_Programacion_Orientada_a_Objetos-POO/Python/main.py
+++ Curso_de_Programacion_Orientada_a_Objetos-POO/Python/main.py
@@ -1,13 +1,14 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
   print("Hola mundo")
-  car1 = Car()
-  car1.license = "KHX416"
-  car1.driver = "Fabio Escobar"
+  
+  car1 = Car("KHX416", Account("Fabio Escobar", "1140"))
   print(vars(car1))
+  print(vars(car1.driver))
 
-  car2 = Car()
-  car2.license = "XHK416"
-  car2.driver = "Alejandro Escobar"
-  print(vars(car2))
+  # car2 = Car()
+  # car2.license = "XHK416"
+  # car2.driver = "Alejandro Escobar"
+  # print(vars(car2))
