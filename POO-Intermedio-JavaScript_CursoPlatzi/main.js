cf3b7fa0e3bf7df489df21c23548a7cedc059c07
fabztech <fabztech@outlook.com>
2022-10-08 16:24:50 -0500

    utilizando metodos del prototipo object

---
+++ POO-Intermedio-JavaScript_CursoPlatzi/main.js
@@ -0,0 +1,14 @@
+const fabio = {
+	name: "Fabio",
+	age: 32,
+	approvedCourses: ["Curso 1"],
+	addCourse(newCourse) {
+		console.log("This", this);
+		console.log("This.approveCourses", this.approvedCourses);
+		this.approvedCourses.push(newCourse);
+	},
+};
+// console.log(Object.keys(fabio));
+// console.log(Object.getOwnPropertyNames(fabio));
+// console.log(Object.entries(fabio));
+console.log(Object.getOwnPropertyDescriptors(fabio));
