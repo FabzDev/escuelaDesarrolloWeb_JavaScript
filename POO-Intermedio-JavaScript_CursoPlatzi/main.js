08e962d616dd8a34277bd1d60e395c041616f22b
fabztech <fabztech@outlook.com>
2022-10-10 21:10:50 -0500

    recorriendo array con for

--- POO-Intermedio-JavaScript_CursoPlatzi/main.js
+++ POO-Intermedio-JavaScript_CursoPlatzi/main.js
@@ -1,22 +1,37 @@
-const fabio = {
-	name: "Fabio",
-	age: 32,
-	approvedCourses: ["Curso 1"],
-	addCourse(newCourse) {
-		console.log("This", this);
-		console.log("This.approveCourses", this.approvedCourses);
-		this.approvedCourses.push(newCourse);
-	},
-};
+// const fabio = {
+// 	name: "Fabio",
+// 	age: 32,
+// 	approvedCourses: ["Curso 1"],
+// 	addCourse(newCourse) {
+// 		console.log("This", this);
+// 		console.log("This.approveCourses", this.approvedCourses);
+// 		this.approvedCourses.push(newCourse);
+// 	},
+// };
 
-const obj1 = {
-	a: "a",
-	b: "b",
-	c: {
-		d: "d",
-		e: "e",
-	},
-};
+// const obj1 = {
+// 	a: "a",
+// 	b: "b",
+// 	c: {
+// 		d: "d",
+// 		e: "e",
+// 	},
+// };
 
-const stringifiedComplexObj = JSON.stringify(obj1);
-const obj2 = JSON.parse(stringifiedComplexObj);
+// const stringifiedComplexObj = JSON.stringify(obj1);
+// const obj2 = JSON.parse(stringifiedComplexObj);
+
+// function recursiva(){
+// 	if (/* validación*/){
+// 		// llamados recursivos
+// 	} else {
+// 		// llamados normales, sin recursividad
+// 	}
+// }
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 5665, 2, 5, 5, 41, 2, 3, 6, 5];
+let numero = 0;
+
+for (let index = 0; index < numeros.length; index++) {
+	numero = numeros[index];
+	console.log({ index, numero });
+}
