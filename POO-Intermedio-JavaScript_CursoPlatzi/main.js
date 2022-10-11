88cd3d4967a3c912161e30bfab829ae72b7fd979
fabztech <fabztech@outlook.com>
2022-10-10 21:21:37 -0500

    recorriendo array con recursividad

--- POO-Intermedio-JavaScript_CursoPlatzi/main.js
+++ POO-Intermedio-JavaScript_CursoPlatzi/main.js
@@ -31,7 +31,18 @@
 const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 5665, 2, 5, 5, 41, 2, 3, 6, 5];
 let numero = 0;
 
-for (let index = 0; index < numeros.length; index++) {
-	numero = numeros[index];
-	console.log({ index, numero });
+// for (let index = 0; index < numeros.length; index++) {
+// 	numero = numeros[index];
+// 	console.log({ index, numero });
+// }
+
+function recursiva(numbersArray) {
+	if (numbersArray.length != 0) {
+		const firstNum = numbersArray[0];
+		console.log(firstNum);
+		numbersArray.shift();
+		return recursiva(numbersArray);
+	}
 }
+
+recursiva(numeros);
