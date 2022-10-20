0a65cba2df4bf1916bf366208499cca6f54b3c44
fabztech <fabztech@outlook.com>
2022-10-19 19:46:53 -0500

    ECMAScript - ES6 - let const V2.0 final

--- ECMAScript-Historia/es6/00-let-cons.js
+++ ECMAScript-Historia/es6/00-let-cons.js
@@ -1,3 +1,20 @@
 var lastName = "David";
-lastName = "Oscar";
-console.log(lastName);
+
+let fruit = "Apple";
+fruit = "Kiwi";
+console.log(fruit);
+
+const animal = "Dog";
+// animal = "Cat";
+console.log(animal);
+
+const fruits = () => {
+	if (true) {
+		var fruit1 = "apple"; // function scope
+		let fruit2 = "kiwi"; // block scope
+		const fruit3 = "banana"; // block scope
+	}
+	console.log({ fruit1, fruit2, fruit3 });
+};
+
+fruits();
