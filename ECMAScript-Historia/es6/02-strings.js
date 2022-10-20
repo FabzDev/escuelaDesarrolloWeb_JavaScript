055f04a12208f6abacf66ee9f843a985450529a0
fabztech <fabztech@outlook.com>
2022-10-19 20:14:42 -0500

    ECMAScript - ES6 - Strings completed // default parameters

---
+++ ECMAScript-Historia/es6/02-strings.js
@@ -0,0 +1,16 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+
+let epicPhrase2 = `${hello} ${world}!, esto esta genial`;
+
+console.log(epicPhrase2);
+
+// Multi-line Strings
+
+let lorem = "Esto es un string \n" + "esto es otra linea";
+console.log(lorem);
+
+let lorem2 = `Esto es un string
+esta es la continuacion de la linea anterior`;
+console.log(lorem2);
