cc9a291dd9e65ae957afa389579c1f6e077d1e00
fabztech <fabztech@outlook.com>
2022-10-20 05:11:16 -0500

    ECMAScript - ES6 - Spread operator -F // Object Literal -B

--- ECMAScript-Historia/es6/04-rest-spread.js
+++ ECMAScript-Historia/es6/04-rest-spread.js
@@ -9,3 +9,21 @@ console.log(a, fruits[1]);
 let user = { username: "Fabio", age: 32 };
 let { username, age } = user;
 console.log(username, user.age);
+
+// spead operator
+
+let person = { name: "Fabio", age: 32 };
+let country = "CO";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+	console.log(values);
+	console.log(num + values[1]);
+	return num + values[1];
+}
+
+sum(1, 1, 3, 3);
