b4b665c9c9c3822975cfa11cb4dfcdbd5d1006c9
fabztech <fabztech@outlook.com>
2022-10-19 20:51:53 -0500

    ECMAScript - ES6 - Rest Spread finished // spread operator begining

---
+++ ECMAScript-Historia/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// array destructuring
+
+let fruits = ["Apple", "Banana"];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+// object destructuring
+
+let user = { username: "Fabio", age: 32 };
+let { username, age } = user;
+console.log(username, user.age);
