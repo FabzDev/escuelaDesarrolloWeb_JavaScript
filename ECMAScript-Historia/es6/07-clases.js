6632a6385aae8607c8f5879249108082a7636a94
fabztech <fabztech@outlook.com>
2022-10-20 20:56:15 -0500

    ECMAScript - ES6 - Clases V.Final

--- ECMAScript-Historia/es6/07-clases.js
+++ ECMAScript-Historia/es6/07-clases.js
@@ -14,3 +14,62 @@ const fabio04 = new user();
 console.log(fabio04.greeting());
 const yuliet2horta = new user();
 console.log(yuliet2horta.greeting());
+
+// constructor
+
+class user {
+	// Constructor
+	constructor() {
+		console.log("Nuevo Usuario");
+	}
+	greeting() {
+		return "Hello World!";
+	}
+}
+
+const alejandro02 = new user();
+
+// this
+class user {
+	constructor(name) {
+		this.name = name;
+	}
+	// metodos
+	speak() {
+		return "Hello";
+	}
+	greeting() {
+		return `${this.speak()} ${this.name}!`;
+	}
+}
+
+const laurita = new user("Laura");
+console.log(laurita.greeting());
+
+// setters and getters
+
+class user {
+	// constructor
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	// metodos
+	speak() {
+		return "Hello";
+	}
+	greeting() {
+		return `${this.speak()} ${this.name}!`;
+	}
+	get uAge() {
+		return this.age;
+	}
+
+	set uAge(n) {
+		this.age = n;
+	}
+}
+
+const mrButi = new user("Fabio", 32);
+console.log(mrButi.uAge);
+console.log((mrButi.uAge = 20));
