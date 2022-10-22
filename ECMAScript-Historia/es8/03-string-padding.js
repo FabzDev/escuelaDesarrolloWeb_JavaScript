75089e71647d59d09cecd3f38ae574da4483e2d4
fabztech <fabztech@outlook.com>
2022-10-22 08:18:54 -0500

    ECMAScript - ES8: trailing commas

--- ECMAScript-Historia/es8/03-string-padding.js
+++ ECMAScript-Historia/es8/03-string-padding.js
@@ -1,4 +1,4 @@
 const texto = "Hello";
 
-console.log(texto.padStart(texto.length + 1, "_"));
-console.log(texto.padEnd(texto.length + 1, "_"));
+console.log(texto.padStart(texto.length + 1, "_")); //_Hello
+console.log(texto.padEnd(texto.length + 1, "_")); //Hello_
---
+++ ECMAScript-Historia/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+const edades = [21, 23, 28, 30, 31, , 33, , , 38]; // edades a las que consegui empleo
+
+console.log(edades); // [ 21, 23, 28, 30, 31, <1 empty item>, 33, <2 empty items>, 38 ]
+console.log(edades.length); //10
