2103af800d1403a2d86012e2e7f69fddbf28e433
fabztech <fabztech@outlook.com>
2022-10-22 11:38:06 -0500

    ECMAScript - ES10: fromEntries -> de Arrays a objetos

---
+++ ECMAScript-Historia/es10/03-fromEntries.js
@@ -0,0 +1,14 @@
+const entries = new Map([
+	["name", "oscar"],
+	["age", 34],
+]);
+console.log(entries); //Map { 'name' => 'oscar', 'age' => 34 }
+console.log(Object.fromEntries(entries)); // { name: 'oscar', age: 34 }
+
+const entries2 = [
+	["name", "oscar"],
+	["age", 34],
+];
+console.log(entries2); //[ [ 'name', 'oscar' ], [ 'age', 34 ] ]
+console.log(Object.fromEntries(entries2)); // { name: 'oscar', age: 34 }
+//{[papa: "Fabio", mama: "Yulieth", hijo: "Alejandro"], tia: "Laurita", tio: "Giova"};
