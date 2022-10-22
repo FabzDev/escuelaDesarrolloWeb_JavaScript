5a6eefccaafeec9271e67166b530c8c40d478d29
fabztech <fabztech@outlook.com>
2022-10-22 13:27:14 -0500

    ECMAScript - ES11: promiseAllSettled

--- ECMAScript-Historia/es11/01-bigInt.js
+++ ECMAScript-Historia/es11/01-bigInt.js
@@ -1,5 +1,4 @@
-const aBigNumber = 5231231231232131234; // convencion original para valores numericos muy grandes
-// const anotherBigNumber = BigInt(454645156168461546);
+const aBigNumber = 454645156168461546n; // convencion original para valores numericos muy grandes
+const anotherBigNumber = BigInt(454645156168461546);
 console.log(aBigNumber);
-// console.log(anotherBigNumber);
-const number = 5;
+console.log(anotherBigNumber);
