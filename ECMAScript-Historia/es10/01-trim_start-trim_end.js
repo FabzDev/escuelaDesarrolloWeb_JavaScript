99061c031b6cebfb45dbea905ef72cbde2a61914
fabztech <fabztech@outlook.com>
2022-10-22 11:25:32 -0500

    ECMAScript - ES10: try catch

--- ECMAScript-Historia/es10/01-trim_start-trim_end.js
+++ ECMAScript-Historia/es10/01-trim_start-trim_end.js
@@ -1,4 +1,4 @@
 const hello = "          holi            my friend         ";
 console.log(hello); //          holi            my friend            .
 console.log(hello.trimStart()); // holi            my friend         .
-console.log(hello.trimEnd()); // holi            my friend
+console.log(hello.trimEnd()); //          holi            my friend.
