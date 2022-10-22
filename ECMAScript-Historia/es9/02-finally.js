1e1ac2a7ffe18f80ae8a747b23fab80dde20edfa
fabztech <fabztech@outlook.com>
2022-10-22 09:27:58 -0500

    ECMAScript - ES9: finally

---
+++ ECMAScript-Historia/es9/02-finally.js
@@ -0,0 +1,14 @@
+const anotherFunction = () => {
+	return new Promise((res, rej) => {
+		if (true) {
+			res("Hey!!");
+		} else {
+			rej("ugh...");
+		}
+	});
+};
+
+anotherFunction()
+	.then((response) => console.log(response))
+	.catch((err) => console.log(err))
+	.finally(() => console.log("Listo!"));
