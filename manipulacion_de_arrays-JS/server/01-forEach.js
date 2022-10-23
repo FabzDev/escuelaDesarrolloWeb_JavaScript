ea776ad51fe3a0c94ec1da21be8979b316f2add6
fabztech <fabztech@outlook.com>
2022-10-23 11:46:16 -0500

    Manipulacion de arrays - map

--- manipulacion_de_arrays-JS/server/01-forEach.js
+++ manipulacion_de_arrays-JS/server/01-forEach.js
@@ -1,3 +1,9 @@
 const letters = ["a", "b", "c"];
 
 letters.forEach((eachValue) => console.log("forEach " + eachValue));
+
+const array = [2, 4, 5, 6];
+
+const newArray = array.map((eachValue) => eachValue * 2);
+
+console.log(newArray);
