505fcc56ee7b0a446ff989306c61204fc2824e0a
fabztech <fabztech@outlook.com>
2022-10-24 22:17:10 -0500

    Manipulacion de arrays - Array.reduceReloaded reto Fabio

--- manipulacion_de_arrays-JS/server/06-reduce.js
+++ manipulacion_de_arrays-JS/server/06-reduce.js
@@ -1,8 +1,6 @@
 // Reduce reloaded
-
 const arrayReloaded = [1, 3, 2, 3, 3, 1, 10];
-
-const rta = arrayReloaded.reduce((obj, item) => {
+const rta5 = arrayReloaded.reduce((obj, item) => {
 	if (obj[item]) {
 		obj[item] += 1;
 	} else {
@@ -11,9 +9,70 @@ const rta = arrayReloaded.reduce((obj, item) => {
 	return obj;
 }, {});
 
-console.log(rta);
+console.log(rta5);
 
 // output
 // 1: 1,
 // 3: 2,
 // 2: 1
+
+const data = [
+	{
+		name: "Nicolas",
+		level: "low",
+	},
+	{
+		name: "Andrea",
+		level: "medium",
+	},
+	{
+		name: "Zulema",
+		level: "high",
+	},
+	{
+		name: "Santiago",
+		level: "low",
+	},
+	{
+		name: "Valentina",
+		level: "medium",
+	},
+	{
+		name: "Lucia",
+		level: "low",
+	},
+];
+
+const rta = data.reduce((acum, item) => {
+	//case low
+	if (item["level"] == "low") {
+		if (!acum["Low"]) {
+			acum["Low"] = 1;
+		} else {
+			acum["Low"] += 1;
+		}
+	}
+	//case medium
+	if (item["level"] == "medium") {
+		if (!acum["Medium"]) {
+			acum["Medium"] = 1;
+		} else {
+			acum["Medium"] += 1;
+		}
+	}
+	//case high
+	if (item["level"] == "high") {
+		if (!acum["High"]) {
+			acum["High"] = 1;
+		} else {
+			acum["High"] += 1;
+		}
+	}
+	return acum;
+}, {});
+
+console.log(rta);
+// OUTPUT
+// low: 3
+// medium: 2
+// high: 4
