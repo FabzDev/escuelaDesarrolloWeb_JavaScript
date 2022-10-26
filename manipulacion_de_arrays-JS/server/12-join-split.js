e1883928820c5a30775c4679cb3e074f47872406
fabztech <fabztech@outlook.com>
2022-10-26 06:31:37 -0400

    Manipulacion de arrays - reto

--- manipulacion_de_arrays-JS/server/12-join-split.js
+++ manipulacion_de_arrays-JS/server/12-join-split.js
@@ -2,4 +2,14 @@ const elements = ["Fire", "Air", "Water"];
 
 const separator = "--";
 const joined = elements.join(separator);
-console.log(joined);
+
+const title = "Curso de manipulación de arrays";
+const splited = title.split(" ").join("-").toLocaleLowerCase();
+console.log(splited);
+
+function solution(title) {
+	return title.toLowerCase().split(" ").join("-");
+}
+console.log(solution("La forma de correr Python"));
+console.log(solution("La API para nunca parar de aprender"));
+console.log(solution("Curso de arrays"));
