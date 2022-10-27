5060496731dcba9eca8db58ffa9e2868d2a698e4
fabztech <fabztech@outlook.com>
2022-10-26 21:35:41 -0400

    Manipulacion de arrays - flat - reto flat recursivo

--- manipulacion_de_arrays-JS/server/14-flat.js
+++ manipulacion_de_arrays-JS/server/14-flat.js
@@ -10,7 +10,24 @@ for (vector of matriz) {
 		newMatriz.push(item);
 	}
 }
-console.log("con for: ", newMatriz);
+// console.log("con for: ", newMatriz);
 
-const newMatriz2 = matriz.flat();
-console.log("con flat: ", newMatriz2);
+// const newMatriz2 = matriz.flat();
+// console.log("con flat: ", newMatriz2);
+
+// flat con recursividad
+
+const matrizMakia = [[1, 2, [3, 4, [11, 12]]], 5, 6, [7, 8], 9, 0];
+// const newArray = [];
+function flatHechiza(matriz) {
+	let newArray = [];
+	for (vector of matriz) {
+		if (Array.isArray(vector)) {
+			newArray = newArray.concat(flatHechiza(vector));
+		} else {
+			newArray.push(vector);
+		}
+	}
+	return newArray;
+}
+console.log(flatHechiza(matrizMakia));
