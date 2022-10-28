6183a3acfe8f15e68bac7c1a5623c42c1c1c3592
fabztech <fabztech@outlook.com>
2022-10-27 23:01:21 -0400

    Manipulacion de arrays - mutable - reto completado

--- manipulacion_de_arrays-JS/server/16-mutable.js
+++ manipulacion_de_arrays-JS/server/16-mutable.js
@@ -1,14 +1,50 @@
-const products = [
-	{ title: "Pizza", price: 121, id: "🍕" },
-	{ title: "Burger", price: 121, id: "🍔" },
-	{ title: "Hot cakes", price: 121, id: "🥞" },
+// const products = [
+// 	{ title: "Pizza", price: 121, id: "🍕" },
+// 	{ title: "Burger", price: 121, id: "🍔" },
+// 	{ title: "Hot cakes", price: 121, id: "🥞" },
+// ];
+
+// const carrito = (productID, foodData) =>
+// 	foodData.filter((food) => food.id == productID);
+
+// const stock = (productID, foodData) =>
+// 	foodData.filter((food) => food.id != productID);
+
+// console.log("Carrito: ", carrito("🍔", products));
+// console.log("Stock: ", stock("🍔", products));
+
+const productsV2 = [
+	{ title: "Pizza", price: 120, id: "🍕" },
+	{ title: "Burger", price: 160, id: "🍔" },
+	{ title: "Hot cakes", price: 80, id: "🥞" },
 ];
 
-const carrito = (productID, foodData) =>
-	foodData.filter((food) => food.id == productID);
+const changes = {
+	id: "🥞",
+	changes: {
+		price: 120,
+		description: " USA",
+	},
+};
 
-const stock = (productID, foodData) =>
-	foodData.filter((food) => food.id != productID);
+// const prueba = {productsV2}
+// const productV3 = productsV2.find((object) => object.id == update.id);
+// const productV4 = { ...productV3, ...update };
+// console.log(productV4);
 
-console.log("Carrito: ", carrito("🍔", products));
-console.log("Stock: ", stock("🍔", products));
+// IN: update
+// OUT: productsVF
+
+function solution(update) {
+	const productIndex = productsV2.findIndex((item) => item.id == update.id);
+	const productToUpdate = productsV2.find((item) => item.id == update.id);
+	const updatedProduct = { ...productToUpdate, ...update.changes };
+	const replacedProduct = productsV2.splice(productIndex, 1, updatedProduct);
+	return productsV2;
+}
+
+// { title: 'Pizza', price: 121, id: '🍕' }
+// { title: 'Pizza', price: 200, id: '🍕', description: "delicious", }
+//     if (typeof array === 'object')}
+
+console.log(solution(changes));
