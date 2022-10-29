75922567c0cd13ad3668ee701abaf24677339e06
fabztech <fabztech@outlook.com>
2022-10-29 15:30:30 -0400

    Estructuras de datos con JavaScript - Hash Tables - Set  by: profe-platzi V2.0

--- estructuras-de-datos-js_curso-platzi/hash-table.js
+++ estructuras-de-datos-js_curso-platzi/hash-table.js
@@ -5,18 +5,19 @@ class HashTable {
 	hashMethod(key) {
 		let hash = 0;
 		for (let i = 0; i < key.length; i++) {
-			hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+			hash = (hash + key.charCodeAt(i) * i) % this.bucket.length;
 		}
 		return hash;
 	}
 
 	insert(key, value) {
-		const address = this.hashMetod[key];
+		const address = this.hashMethod(key);
 		if (!this.bucket[address]) {
 			this.bucket[address] = [];
-		} else {
-			this.bucket[address].push([key, value]);
 		}
-		return this.data;
+		this.bucket[address].push([key, value]);
+		return this.bucket;
 	}
 }
+const test = new HashTable(100);
+test.insert("Fabio", 1990);
