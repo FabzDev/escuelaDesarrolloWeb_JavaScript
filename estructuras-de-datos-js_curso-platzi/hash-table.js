111f4a4999567d9d4d10c98fb0a5a7c1f7aea41c
fabztech <fabztech@outlook.com>
2022-10-29 16:11:37 -0400

    Estructuras de datos con JavaScript - Hash Tables - Set  by: Fabio v2.0

--- estructuras-de-datos-js_curso-platzi/hash-table.js
+++ estructuras-de-datos-js_curso-platzi/hash-table.js
@@ -18,6 +18,21 @@ class HashTable {
 		this.bucket[address].push([key, value]);
 		return this.bucket;
 	}
+	get(key) {
+		for (let i = 0; i < this.bucket.length; i++) {
+			if (this.bucket[i]) {
+				if (this.bucket[i][this.bucket[i].length - 1][0] == key) {
+					console.log(i);
+					return i;
+				}
+			}
+		}
+	}
 }
 const test = new HashTable(100);
 test.insert("Fabio", 1990);
+test.insert("Yulieth", 1996);
+test.insert("Alejandro", 2023);
+test.insert("Laurita", 1995);
+test.insert("Giova", 2001);
+test.insert("Fabio Fernando", 1966);
