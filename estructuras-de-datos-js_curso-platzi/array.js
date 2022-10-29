131375389b3365c20b7da33b3a3ef7cf61211144
fabztech <fabztech@outlook.com>
2022-10-28 23:52:09 -0400

    Estructuras de datos con JavaScript - Array.push, Array.pop, Array.delete

---
+++ estructuras-de-datos-js_curso-platzi/array.js
@@ -0,0 +1,30 @@
+class MyArray {
+	constructor() {
+		this.length = 0;
+		this.data = {};
+	}
+
+	get(index) {
+		return this.data[index];
+	}
+	push(item) {
+		this.data[this.length] = item;
+		this.length++;
+		return this.data;
+	}
+	pop() {
+		const lastItem = this.data[this.length - 1];
+		delete this.data[this.length - 1];
+		this.length--;
+		return lastItem;
+	}
+	delete(index) {
+		for (let i = index; i < this.length - 1; i++) {
+			this.data[i] = this.data[i + 1];
+		}
+		this.pop();
+		return this.data;
+	}
+}
+
+const nombres = new MyArray();
