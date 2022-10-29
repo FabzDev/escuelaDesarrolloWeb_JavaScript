819d4a9e785cbbb9235588c81e26458608abb1d1
fabztech <fabztech@outlook.com>
2022-10-29 00:46:20 -0400

    Estructuras de datos con JavaScript - Array.push, Array.pop, Array.delete, Reto completado, metodos shift y unshift

--- estructuras-de-datos-js_curso-platzi/array.js
+++ estructuras-de-datos-js_curso-platzi/array.js
@@ -21,15 +21,32 @@ class MyArray {
 	delete(index) {
 		const item = this.data[index];
 		this.shiftIndex(index);
+		delete this.data[this.length - 1];
+		this.length--;
 		return item;
 	}
 	shiftIndex(index) {
 		for (let i = index; i < this.length - 1; i++) {
 			this.data[i] = this.data[i + 1];
 		}
+	}
+	shift() {
+		const firstItem = this.data[0];
+		this.shiftIndex(0);
 		delete this.data[this.length - 1];
 		this.length--;
+		return firstItem;
+	}
+	unshift(item) {
+		this.unshiftIndex();
+		this.data[0] = item;
+		this.length++;
+		return this.data;
+	}
+	unshiftIndex() {
+		for (let i = this.length; i > 0; i--) {
+			this.data[i] = this.data[i - 1];
+		}
 	}
 }
-
 const nombres = new MyArray();
