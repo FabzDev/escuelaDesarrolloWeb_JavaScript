ae39a1f477626e4fccb7cd81e2f65076506a6cef
fabztech <fabztech@outlook.com>
2022-10-30 19:41:17 -0400

    Estructuras de datos con JavaScript - linked list - referencia a objetos entendido

--- estructuras-de-datos-js_curso-platzi/03-single-linked-list.js
+++ estructuras-de-datos-js_curso-platzi/03-single-linked-list.js
@@ -1,20 +1,20 @@
 // 1 --> 2 --> 3 --> 4 --> 5--> null
 
-let singlyLinkedList = {
-	head: {
-		value: 1,
-		next: {
-			value: 2,
-			next: {
-				value: 3,
-				next: {
-					value: 4,
-					next: null,
-				},
-			},
-		},
-	},
-};
+// let singlyLinkedList = {
+// 	head: {
+// 		value: 1,
+// 		next: {
+// 			value: 2,
+// 			next: {
+// 				value: 3,
+// 				next: {
+// 					value: 4,
+// 					next: null,
+// 				},
+// 			},
+// 		},
+// 	},
+// };
 
 class Nodo {
 	constructor(value) {
@@ -32,19 +32,18 @@ class MySinglyLinkedList {
 		this.tail = this.head;
 		this.length = 1;
 	}
+
 	append(value) {
-		this.tail = value;
+		const newNode = new Nodo(value);
+		this.head.next = newNode;
+		this.head = newNode;
 		this.length++;
-		if (Array.isArray(this.next)) {
-			this.append(value);
-		} else {
-			this.value = value;
-			this.next = null;
-		}
+
+		return this;
 	}
 }
 
-const list = new MySinglyLinkedList(2);
+let list = new MySinglyLinkedList(1);
+list.append(2);
+list.append(3);
 list.append(4);
-list.append(8);
-list.append(16);
