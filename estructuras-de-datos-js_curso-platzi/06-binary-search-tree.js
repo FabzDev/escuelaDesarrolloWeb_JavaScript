68247050141650a515c7a9fc4585fc18572c2495
fabztech <fabztech@outlook.com>
2022-11-04 07:39:42 -0400

    Estructuras de datos con JavaScript - Binary search trees V1.2 by Profe (incompleto)

--- estructuras-de-datos-js_curso-platzi/06-binary-search-tree.js
+++ estructuras-de-datos-js_curso-platzi/06-binary-search-tree.js
@@ -8,34 +8,19 @@ class Node {
 
 class BinaryTree {
 	constructor() {
-		this.head = {
-			value: null,
-			prev: null,
-			nextUp: null,
-			nextDown: null,
-		};
-		this.tail = this.head;
-		this.length = 0;
-		return this;
+		this.root = null;
 	}
 
-	add(value) {
-		if (this.length == 0) {
-			const firstNode = new Node(value);
-			this.head = firstNode;
-			this.tail = firstNode;
-
-			this.length++;
-			return this;
-		} else if (this.length == 1) {
-			const nodeUp = new Node(value);
-			const nodeDown = new Node(value);
-			this.tail.nextUp = nodeUp;
-			this.tail.nextDown = nodeDown;
-			this.nodeUp.prev = this.tail;
-			this.nodeDown.prev = this.tail;
-			this.tail = nodwUp;
-		} else if (this.length > 1) {
+	insert(value) {
+		const newNode = new Node(value);
+		if (this.root === null) {
+			this.root = newNode;
+		} else {
+			let currentNode = this.root;
+			while (true) {
+				if (value < currentNode) {
+				}
+			}
 		}
 	}
 }
