4a1c7c8dd72cbe737fff7fc96c381b72b90cac2f
fabztech <fabztech@outlook.com>
2022-11-06 00:57:16 -0500

    Curso complejidad algoritmica JS - Complejidad espacial

--- complejidad-temporal_curso-platzi/03-complejidad-espacial.js
+++ complejidad-temporal_curso-platzi/03-complejidad-espacial.js
@@ -1,1 +1,25 @@
 // Complejidad espacial
+function contar(n) {
+	for (let index = 1; index <= n; index++) {
+		console.log(index);
+	}
+}
+
+function repetir(arreglo) {
+	const nuevoArreglo = arreglo;
+}
+
+function convertirAString(arreglo) {
+	const arString = arreglo.map((elemento) => elemento.toString());
+	return arString;
+}
+
+function dosDimensiones(valor) {
+	let x = new Array(valor);
+	for (let i = 0; i < valor; i++) {
+		x[i] = new Array(valor);
+		for (let j = 0; j < valor; j++) {
+			x[i][j] = 0;
+		}
+	}
+}
