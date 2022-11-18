4aac83e8e2a693f12b4f4b580c50924bf674a1bf
fabztech <fabztech@outlook.com>
2022-11-18 07:40:41 -0500

    Curso de Asincronismo con JavaScript - Callbacks (C6) V2.0

--- asincronismo-con-javascript/index.js
+++ asincronismo-con-javascript/index.js
@@ -7,3 +7,11 @@ function calc(num1, num2, callback) {
 }
 
 console.log(calc(2, 2, sum));
+
+setTimeout(() => console.log("Hola JavaScript"), 1000);
+
+function saludo(nombre) {
+	console.log(`Hola ${nombre}`);
+}
+
+setTimeout(() => saludo("Fabio"), 2000);
