3c15e1f0ea04cc6f4592e2a8f5a7fd2283bd0f1c
fabztech <fabztech@outlook.com>
2022-11-19 00:18:26 -0500

    Curso de Asincronismo con JavaScript - Fetch (C13) Entendido

--- asincronismo-con-javascript/src/promise/challenge.js
+++ asincronismo-con-javascript/src/promise/challenge.js
@@ -5,7 +5,21 @@ function dataFetch(urlApi) {
 	return fetch(urlApi);
 }
 
+// dataFetch(`${API}/products`)
+// 	.then((response) => response.json())
+// 	.then((response) => console.log(response[0]))
+// 	.then(console.log("OLE!"));
+
 dataFetch(`${API}/products`)
 	.then((response) => response.json())
-	.then((response) => console.log(response[0]))
-	.then(console.log("OLE!"));
+	.then((products) => {
+		return dataFetch(`${API}/products/${products[3].id}`);
+	})
+	.then((response) => response.json())
+	.then((product) => {
+		console.log(product);
+		console.log(product.title);
+		return dataFetch(`${API}/categories/${product.category.id}`);
+	})
+	.then((response) => response.json())
+	.then((category) => console.log(category.name));
