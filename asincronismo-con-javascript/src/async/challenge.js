c566c0e388f2a2a44049085dadcc708044ec4cad
fabztech <fabztech@outlook.com>
2022-11-19 20:32:54 -0500

    Curso de Asincronismo con JavaScript - try catch C16

--- asincronismo-con-javascript/src/async/challenge.js
+++ asincronismo-con-javascript/src/async/challenge.js
@@ -2,13 +2,26 @@ import fetch from "node-fetch";
 const API = "https://api.escuelajs.co/api/v1";
 
 async function fetchData(urlApi) {
-	const rawData = await fetch(`${urlApi}/products`);
+	const rawData = await fetch(`${urlApi}`);
 	const data = await rawData.json();
 	return data;
 }
 
-const dataBase = fetchData(API);
+// const dataBase = fetchData(API);
+// const dataBase2 = await dataBase;
+// console.log(dataBase2[5]);
 
-const dataBase2 = await dataBase;
+const anotherFn = async (urlApi) => {
+	try {
+		const products = await fetchData(`${urlApi}/products`);
+		const product = await fetchData(`${urlApi}/products/${products[140].id}`);
+		const category = await fetchData(`${urlApi}/categories/${product.category.id}`);
+		// console.log(products);
+		console.log(product.title);
+		console.log(category);
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-console.log(dataBase2[5]);
+anotherFn(API);
