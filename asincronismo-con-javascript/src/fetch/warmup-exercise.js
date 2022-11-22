a6342ea242b7642be1672b184f3727080a61898d
fabztech <fabztech@outlook.com>
2022-11-22 13:58:42 -0500

    Fetch with Nader - Warmup-exercise.js done

--- asincronismo-con-javascript/src/fetch/warmup-exercise.js
+++ asincronismo-con-javascript/src/fetch/warmup-exercise.js
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 /*
     1. Install node-fetch if your Node version is less than 18
        using NPM, and import it in to your file (ES Module style)
@@ -7,3 +8,13 @@
     3. Compare what you see in Node to visiting the actual page in
        your Browser - what matches up?
 */
+
+const craigUrl = "https://www.craigslist.org/about/";
+
+const fetchCraiglist = async () => {
+	const response = await fetch(craigUrl);
+	const html = await response.text();
+	console.log(html);
+};
+
+fetchCraiglist();
