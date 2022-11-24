99e563a3cd9108637ab80a1833852a87cd6262e2
fabztech <fabztech@outlook.com>
2022-11-23 23:11:08 -0500

    Curso basico consumo API REST - The Dog API V2.0

--- curso-basico-api-rest_javascript/main.js
+++ curso-basico-api-rest_javascript/main.js
@@ -1,10 +1,24 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
-const URL = "https://api.thedogapi.com/v1/images/search";
-const img = document.querySelector("img");
+const URL = "https://api.thedogapi.com/v1/images/search?limit=2";
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+const img3 = document.querySelector("#img3");
+const img4 = document.querySelector("#img4");
+const btn = document.querySelector(".boton");
 
-fetch(URL)
-	.then((rawData) => rawData.json())
-	.then((jsonData) => {
-		img.src = jsonData[0].url;
-	});
+(async () => {
+	const rawData = await fetch(URL);
+	const jsonData = await rawData.json();
+	console.log(jsonData);
+	img1.src = jsonData[0].url;
+	img2.src = jsonData[1].url;
+	img3.src = jsonData[2].url;
+	img4.src = jsonData[3].url;
+})();
+
+btn.addEventListener("click", reload);
+
+function reload() {
+	window.location.reload();
+}
