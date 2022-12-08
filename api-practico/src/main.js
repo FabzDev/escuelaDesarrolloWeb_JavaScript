c0f88144cf77609cb7a1b56e627f9ef3eb91a38a
fabztech <fabztech@outlook.com>
2022-12-08 14:39:30 -0500

    Curso practico API REST - Preview Tendencias metodo Platzi & metodo Fabio usando MAP

--- api-practico/src/main.js
+++ api-practico/src/main.js
@@ -2,22 +2,38 @@ const API_URL = "https://api.themoviedb.org/3";
 const trendingArticle = document.getElementById("trending-article");
 
 (async function getTrendingMoviedPreview() {
-	// METODO APPEND CHILD BY PLATZI
 	const res = await fetch(`${API_URL}/trending/movie/day?api_key=${API_KEY}`);
 	const data = await res.json();
 	const movies = data.results;
 	console.log(movies);
-	movies.forEach((movie) => {
-		const trendingArticleContainer = document.getElementById("trending-article");
 
-		const movieContainer = document.createElement("div");
-		movieContainer.classList.add("movie-container");
-		trendingArticleContainer.appendChild(movieContainer);
+	// // METODO APPEND CHILD BY PLATZI
+	// movies.forEach((movie) => {
+	// 	const trendingArticleContainer = document.getElementById("trending-article");
 
-		const imgMovie = document.createElement("img");
-		imgMovie.classList.add("movie-img");
-		imgMovie.setAttribute("alt", movie.title);
-		imgMovie.setAttribute("src", "https://image.tmdb.org/t/p/w300/" + movie.poster_path);
-		movieContainer.appendChild(imgMovie);
-	});
+	// 	const movieContainer = document.createElement("div");
+	// 	movieContainer.classList.add("movie-container");
+	// 	trendingArticleContainer.appendChild(movieContainer);
+
+	// 	const imgMovie = document.createElement("img");
+	// 	imgMovie.classList.add("movie-img");
+	// 	imgMovie.setAttribute("alt", movie.title);
+	// 	imgMovie.setAttribute("src", "https://image.tmdb.org/t/p/w300/" + movie.poster_path);
+	// 	movieContainer.appendChild(imgMovie);
+	// });
+
+	// METODO MAP BY FABIO
+	const mapArticle = document.getElementById("trending-article");
+	mapArticle.innerHTML = movies.map(
+		(movie) =>
+			`
+	            <div class="movie-container">
+	            <img
+	                src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+	                class="movie-img"
+	                alt="${movie.title}"
+	            />
+	            </div>
+	        `
+	);
 })();
