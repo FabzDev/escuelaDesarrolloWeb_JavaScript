3a71603d932f897c22c976e0ce210986d589dc44
fabztech <fabztech@outlook.com>
2022-12-08 18:02:47 -0500

    Curso practico API REST - Axios implementado, fetch method comentado"

--- api-practico/src/main.js
+++ api-practico/src/main.js
@@ -11,73 +11,115 @@ const apiAxios = axios.create({
 });
 
 (async function getTrendingMoviedPreview() {
-	const res = await fetch(`${API_URL}/trending/movie/day?api_key=${API_KEY}`);
-	const data = await res.json();
-	const movies = data.results;
+	const res = await apiAxios("/trending/movie/day");
+	// const data = await res.json(); no es necesario en AXIOS
+	const movies = res.data.results;
+	// Tambien es posible usar =>  const { data } = await apiAxios("/trending/movie/day"); para recivir "data" directamente.
 
-	// // METODO APPEND CHILD BY PLATZI
-	// movies.forEach((movie) => {
-	// 	const trendingArticleContainer = document.getElementById("trending-article");
+	// METODO APPEND CHILD BY PLATZI
+	movies.forEach((movie) => {
+		const trendingArticleContainer = document.getElementById("trending-article");
 
-	// 	const movieContainer = document.createElement("div");
-	// 	movieContainer.classList.add("movie-container");
-	// 	trendingArticleContainer.appendChild(movieContainer);
+		const movieContainer = document.createElement("div");
+		movieContainer.classList.add("movie-container");
+		trendingArticleContainer.appendChild(movieContainer);
 
-	// 	const imgMovie = document.createElement("img");
-	// 	imgMovie.classList.add("movie-img");
-	// 	imgMovie.setAttribute("alt", movie.title);
-	// 	imgMovie.setAttribute("src", "https://image.tmdb.org/t/p/w300/" + movie.poster_path);
-	// 	movieContainer.appendChild(imgMovie);
-	// });
-
-	// METODO MAP BY FABIO
-	const mapArticle = document.getElementById("trending-article");
-	mapArticle.innerHTML = movies
-		.map(
-			(movie) =>
-				`
-	            <div class="movie-container">
-	            <img
-	                src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
-	                class="movie-img"
-	                alt="${movie.title}"
-	            />
-	            </div>
-	        `
-		)
-		.join("");
+		const imgMovie = document.createElement("img");
+		imgMovie.classList.add("movie-img");
+		imgMovie.setAttribute("alt", movie.title);
+		imgMovie.setAttribute("src", "https://image.tmdb.org/t/p/w300/" + movie.poster_path);
+		movieContainer.appendChild(imgMovie);
+	});
 })();
 
 (async function getCategoriesPreview() {
-	const res = await fetch(`${API_URL}/genre/movie/list?api_key=${API_KEY}`);
-	const data = await res.json();
+	const { data } = await apiAxios("/genre/movie/list");
 	const genres = data.genres;
 
-	// // METODO APPEND CHILD BY PLATZI
-	// genres.forEach((category) => {
-	// 	const categoriesArticle = document.querySelector("#categoriesPreview .categoriesPreview-list");
+	// METODO APPEND CHILD BY PLATZI
+	genres.forEach((category) => {
+		const categoriesArticle = document.querySelector("#categoriesPreview .categoriesPreview-list");
 
-	// 	const categoryContainer = document.createElement("div");
-	// 	categoryContainer.classList.add("category-container");
-	// 	categoriesArticle.appendChild(categoryContainer);
+		const categoryContainer = document.createElement("div");
+		categoryContainer.classList.add("category-container");
+		categoriesArticle.appendChild(categoryContainer);
 
-	// 	const h3Category = document.createElement("h3");
-	// 	h3Category.classList.add("category-title");
-	// 	h3Category.setAttribute("id", "id" + category.id);
-	// 	h3Category.innerHTML = category.name;
-	// 	categoryContainer.appendChild(h3Category);
-	// });
-
-	// METODO MAP BY FABIO
-	const mapCategory = document.querySelector("#categoriesPreview .categoriesPreview-list");
-	mapCategory.innerHTML = genres
-		.map(
-			(category) =>
-				`
-	            <div class="category-container">
-	            <h3 id="id${category.id}" class="category-title">${category.name}</h3>
-	            </div>
-	        `
-		)
-		.join("");
+		const h3Category = document.createElement("h3");
+		h3Category.classList.add("category-title");
+		h3Category.setAttribute("id", "id" + category.id);
+		h3Category.innerHTML = category.name;
+		categoryContainer.appendChild(h3Category);
+	});
 })();
+
+// (async function getTrendingMoviedPreview() {
+// 	const res = await fetch(`${API_URL}/trending/movie/day?api_key=${API_KEY}`);
+// 	const data = await res.json();
+// 	const movies = data.results;
+
+// 	// // METODO APPEND CHILD BY PLATZI
+// 	// movies.forEach((movie) => {
+// 	// 	const trendingArticleContainer = document.getElementById("trending-article");
+
+// 	// 	const movieContainer = document.createElement("div");
+// 	// 	movieContainer.classList.add("movie-container");
+// 	// 	trendingArticleContainer.appendChild(movieContainer);
+
+// 	// 	const imgMovie = document.createElement("img");
+// 	// 	imgMovie.classList.add("movie-img");
+// 	// 	imgMovie.setAttribute("alt", movie.title);
+// 	// 	imgMovie.setAttribute("src", "https://image.tmdb.org/t/p/w300/" + movie.poster_path);
+// 	// 	movieContainer.appendChild(imgMovie);
+// 	// });
+
+// 	// METODO MAP BY FABIO
+// 	const mapArticle = document.getElementById("trending-article");
+// 	mapArticle.innerHTML = movies
+// 		.map(
+// 			(movie) =>
+// 				`
+// 	            <div class="movie-container">
+// 	            <img
+// 	                src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+// 	                class="movie-img"
+// 	                alt="${movie.title}"
+// 	            />
+// 	            </div>
+// 	        `
+// 		)
+// 		.join("");
+// })();
+
+// (async function getCategoriesPreview() {
+// 	const res = await fetch(`${API_URL}/genre/movie/list?api_key=${API_KEY}`);
+// 	const data = await res.json();
+// 	const genres = data.genres;
+
+// 	// // METODO APPEND CHILD BY PLATZI
+// 	// genres.forEach((category) => {
+// 	// 	const categoriesArticle = document.querySelector("#categoriesPreview .categoriesPreview-list");
+
+// 	// 	const categoryContainer = document.createElement("div");
+// 	// 	categoryContainer.classList.add("category-container");
+// 	// 	categoriesArticle.appendChild(categoryContainer);
+
+// 	// 	const h3Category = document.createElement("h3");
+// 	// 	h3Category.classList.add("category-title");
+// 	// 	h3Category.setAttribute("id", "id" + category.id);
+// 	// 	h3Category.innerHTML = category.name;
+// 	// 	categoryContainer.appendChild(h3Category);
+// 	// });
+
+// 	// METODO MAP BY FABIO
+// 	const mapCategory = document.querySelector("#categoriesPreview .categoriesPreview-list");
+// 	mapCategory.innerHTML = genres
+// 		.map(
+// 			(category) =>
+// 				`
+// 	            <div class="category-container">
+// 	            <h3 id="id${category.id}" class="category-title">${category.name}</h3>
+// 	            </div>
+// 	        `
+// 		)
+// 		.join("");
+// })();
